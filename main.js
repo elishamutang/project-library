5f011b739bbdfcf63d5f529b969514fd8281107e
elishamutang <elishamutang@outlook.com>
2023-11-14 23:33:30 +0800

    Added dialog for Add Book button.

--- main.js
+++ main.js
@@ -5,5 +5,19 @@ function Book() {
 }
 
 function addBookToLibrary() {
-    
+
 }
+
+// Opens up dialog which contains a form to insert book data
+
+const addBook = document.getElementById("addBtn");
+const closeDialog = document.querySelector("dialog > button");
+const dialog = document.querySelector("dialog");
+
+addBook.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+closeDialog.addEventListener("click", () => {
+    dialog.close();
+})
