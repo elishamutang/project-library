fa861798f27012e7a4b797c50bec6be5156e9f1b
elishamutang <elishamutang@outlook.com>
2023-11-15 23:05:56 +1000

    Added event listener for Submit button for modal. Requires more experimentation to make the logic work.

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 const myLibrary = []; // to store book objects
+// const title = document.getElementById("title");
+// const author = document.getElementById("author");
+const submitBtn = document.querySelector("#submit");
+const book = document.querySelector("div.main");
+
+submitBtn.addEventListener("click", addBookToLibrary);
 
 function Book() {
 
 }
 
 function addBookToLibrary() {
+    console.log(e);
+    let bookTitle = document.getElementById("title");
+    let author = document.getElementById("author").value;
 
+    console.log(bookTitle);
 }
 
 // Opens up dialog which contains a form to insert book data
 
 const addBook = document.getElementById("addBtn");
-const closeDialog = document.querySelector("button[type=submit]");
+const closeDialog = document.querySelector("button[formmethod=dialog]");
 const dialog = document.querySelector("dialog");
 
 addBook.addEventListener("click", () => {
